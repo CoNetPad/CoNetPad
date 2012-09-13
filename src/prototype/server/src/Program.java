6b1a499a32b3cdb3ce2308e4a1a0cfe868712419
Cesar Ramirez <cesar.ramsan@gmail.com>
2012-09-13 13:09:07 -0500

    Ready to commit

--- src/prototype/server/src/Program.java
+++ src/prototype/server/src/Program.java
@@ -1,7 +1,6 @@
-import java.io.IOException;
 
 public class Program {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Server server = new Server();
 		server.run();
 	}
