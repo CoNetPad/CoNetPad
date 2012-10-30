c91b554366e272a8e8c70acb3c3a85574818fe32
cesar <ramirez@cesarandres.com>
2012-10-30 18:25:34 -0500

    Transitional folder removed and network files merged to master

--- trans-src/org/ndacm/acmgroup/network/event/MessageReceivedEventListener.java
+++
@@ -1,7 +0,0 @@
-package org.ndacm.acmgroup.network.event;
-
-import java.util.EventListener;
-
-public interface MessageReceivedEventListener extends EventListener {
-	public void MessageReceivedEventOccurred(MessageReceivedEvent evt);
-}
