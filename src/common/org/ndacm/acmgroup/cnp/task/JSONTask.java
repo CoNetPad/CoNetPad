4069879eb69e00d9ee5d782ed7a2980f70d1d0b1
Justin <swordz@gmail.com>
2012-10-30 13:10:29 -0500

    Added some comments.

--- src/common/org/ndacm/acmgroup/cnp/task/JSONTask.java
+++ src/common/org/ndacm/acmgroup/cnp/task/JSONTask.java
@@ -1,11 +1,21 @@
+/**
+ * JSONTask
+ * 
+ * This is a utility class for converting json string to Event object
+ */
+
 package org.ndacm.acmgroup.cnp.task;
 
 import org.ndacm.acmgroup.cnp.TaskType;
 
 public class JSONTask {
-	private String type;
+	private String type;	//the type of json
 	
-	
+	@Override
+	/**
+	 *toString()
+	 *@return String	Override the toString that will return the parameters 
+	 */
 	public String toString()
 	{
 		return type.toString();
--- src/common/org/ndacm/acmgroup/cnp/task/TaskBuilder.java
+++ src/common/org/ndacm/acmgroup/cnp/task/TaskBuilder.java
@@ -1,3 +1,11 @@
+/**
+ * Task Builder Class
+ * 
+ * This is a class that will take a json string convert it to an event object
+ * @author Justin Anderson
+ * @version 1.0
+ */
+
 package org.ndacm.acmgroup.cnp.task;
 
 import org.ndacm.acmgroup.cnp.TaskType;
@@ -8,11 +16,15 @@ import com.google.gson.Gson;
 public class TaskBuilder
 {
 
-	private static String jsonString;
-	private Gson gs;
-	public JSONTask t;
-	private CNPTaskType taskType;
+	private static String jsonString;   //This is the raw form of the string
+	private Gson gs;					//This is the api call to the GSON object
+	public JSONTask t;					//This is the special task for json
+	private CNPTaskType taskType;		//This is the CNPTaskType enumeration
 	
+	/**
+	 * Default Constructor
+	 * @param json	String	This is the json string to convert
+	 */
 	public TaskBuilder(String json)
 	{
 		gs = new Gson();
@@ -30,16 +42,19 @@ public class TaskBuilder
 		
 	}
 	
+	/**
+	 * getCNPTask()
+	 * This will return the Task Object
+	 * @return
+	 */
 	public Task getCNPTask()
 	{
 	
-		String username = "username";
-		String msg = "message";
 		Task ret = null;
 		switch(taskType)
 		{
 			case CHAT:
-				ret =  new ChatTask(username, msg);
+		//		ret =  new ChatTask(username, msg);
 			break;
 			
 		}
