712ec2019df0bc22a8d766c7f1b10b20890a7fa2
Justin <swordz@gmail.com>
2012-11-07 14:57:08 -0600

    Added my stuff for database

--- src/server/org/ndacm/acmgroup/cnp/Account.java
+++ src/server/org/ndacm/acmgroup/cnp/Account.java
@@ -1,10 +1,20 @@
-package server.org.ndacm.acmgroup.cnp;
+package org.ndacm.acmgroup.cnp;
 
 public class Account {
 	
 	private String username;
 	private String email;
 	
+	public Account()
+	{
+		username = "guest";
+		email = "none";
+	}
+	public Account(String uname, String eml)
+	{
+		username = uname;
+		email = eml;
+	}
 	public enum FilePermissionLevel {
 		READ,
 		READ_WRITE,
--- src/server/org/ndacm/acmgroup/cnp/database/IDatabase.java
+++ src/server/org/ndacm/acmgroup/cnp/database/IDatabase.java
@@ -1,26 +1,28 @@
-package server.org.ndacm.acmgroup.cnp.database;
+package org.ndacm.acmgroup.cnp.database;
 
 
-import org.ndacm.acmgroup.cnp.server.*;
+
+
+import java.sql.SQLException;
 
 import server.org.ndacm.acmgroup.cnp.*;
 
 public interface IDatabase {
 
-	Account createAccount(String username, String email, String password);
+	Account createAccount(String username, String email, String password)  throws SQLException;
 	
-	Account retrieveAccount(String username, String password);
-	
-	CNPSession createSession(Account sessionLeader);
-	
-	CNPPrivateSession createSession(Account sessionLeader, String sessionPassword);
-	
-	CNPSession retrieveSession(String sessionName);
-	
-	CNPPrivateSession retrieveSession(String sessionName, String sessionPassword);
-	
-	boolean sessionIsPrivate(String sessionName);
-	
-	boolean createSessionAccount(CNPSession session, Account account,
-			Account.FilePermissionLevel filePermission, Account.ChatPermissionLevel chatPermission);
+//	Account retrieveAccount(String username, String password);
+//	
+//	CNPSession createSession(Account sessionLeader);
+//	
+//	CNPPrivateSession createSession(Account sessionLeader, String sessionPassword);
+//	
+//	CNPSession retrieveSession(String sessionName);
+//	
+//	CNPPrivateSession retrieveSession(String sessionName, String sessionPassword);
+//	
+//	boolean sessionIsPrivate(String sessionName);
+//	
+//	boolean createSessionAccount(CNPSession session, Account account,
+//			Account.FilePermissionLevel filePermission, Account.ChatPermissionLevel chatPermission);
 }
