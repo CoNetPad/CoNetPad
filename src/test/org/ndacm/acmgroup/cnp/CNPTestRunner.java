ddc3fb8a9b4e4a033a3cd08509f729be515a04be
Josh Tan <jtan189@gmail.com>
2012-11-24 17:33:22 -0600

    Fixed build.xml. Everything should work now.

---
+++ src/test/org/ndacm/acmgroup/cnp/CNPTestRunner.java
@@ -0,0 +1,11 @@
+package org.ndacm.acmgroup.cnp;
+
+import org.junit.runner.JUnitCore;
+
+public class CNPTestRunner {
+	
+	public static void main(String[] args) throws Exception {                    
+	       JUnitCore.main("org.ndacm.acmgroup.cnp.CNPTestSuite");            
+	}
+
+}
