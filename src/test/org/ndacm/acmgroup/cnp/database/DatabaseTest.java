66bfbab3da8f95f824db8f9e82950000bc93831a
Josh Tan <jtan189@gmail.com>
2012-11-27 01:29:50 -0600

    Finished the JUnit tests for the Database class. Fixed some errors that were found while writing them as well. Added a JUnit tests folder that will hold the output of 'ant test' (which should run all JUnit4 tests in our project).

--- src/test/org/ndacm/acmgroup/cnp/database/DatabaseTest.java
+++ src/test/org/ndacm/acmgroup/cnp/database/DatabaseTest.java
@@ -1,94 +1,262 @@
 package org.ndacm.acmgroup.cnp.database;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.ndacm.acmgroup.cnp.Account;
+import org.ndacm.acmgroup.cnp.Account.ChatPermissionLevel;
+import org.ndacm.acmgroup.cnp.Account.FilePermissionLevel;
+import org.ndacm.acmgroup.cnp.CNPPrivateSession;
 import org.ndacm.acmgroup.cnp.CNPServer;
 import org.ndacm.acmgroup.cnp.CNPSession;
 import org.ndacm.acmgroup.cnp.exceptions.FailedAccountException;
 import org.ndacm.acmgroup.cnp.exceptions.FailedSessionException;
 
-public class DatabaseTest extends TestCase {
+/**
+ * JUnit test case for Database.
+ *
+ */
+public class DatabaseTest {
 
 	public static CNPServer server;
+	public static CNPSession session1;
+	public static CNPPrivateSession session2;
 	public static Database database;
 	public static Account account1;
+	public static Account account2;
 
-	protected static void setUpBeforeClass() throws Exception {
-		// clear database
-		server = new CNPServer("");
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+		server = new CNPServer(".");
 		database = new Database(server);
-		// database.clearTables();
+		database.clearTables();
 		account1 = database.createAccount("account1", "acc1@test.com", "testme");
-		System.out.println("WTF");
+		account2 = database.createAccount("account2", "acc2@test.com", "testmemore");
+		session1 = database.createSession(103, server);
+		session2 = database.createSession(104, server, "canthackthis");
 	}
 
-	protected static void tearDownAfterClass() throws Exception {
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		database.clearTables();
 	}
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
 	}
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
 	}
 
-	public void testCreateAccount() throws FailedAccountException {
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#createAccount(java.lang.String, java.lang.String, java.lang.String)}.
+	 * @throws FailedAccountException 
+	 */
+	@Test
+	public final void testCreateAccount() throws FailedAccountException {
 		Account account = database.createAccount("john", "john@test.me", "monkey");
 		assertEquals(account.getUsername(),"john");
 		assertEquals(account.getEmail(),"john@test.me");
 	}
 
-	public void testRetrieveAccount() throws FailedAccountException {
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#retrieveAccount(java.lang.String, java.lang.String)}.
+	 * @throws FailedAccountException 
+	 */
+	@Test
+	public final void testRetrieveAccount() throws FailedAccountException {
 		Account retrieved = database.retrieveAccount("account1", "testme");
-		assertEquals(retrieved, account1);
+		assertEquals(retrieved.getUserID(), account1.getUserID());
+		assertEquals(retrieved.getUsername(), account1.getUsername());
+		assertEquals(retrieved.getEmail(), account1.getEmail());
 	}
 
-	public void testCreateSessionIntCNPServer() throws FailedSessionException {
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#createSession(int, org.ndacm.acmgroup.cnp.CNPServer)}.
+	 * @throws FailedSessionException 
+	 */
+	@Test
+	public final void testCreateSessionIntCNPServer() throws FailedSessionException {
 		CNPSession session = database.createSession(100, server);
 		assertEquals(session.getSessionLeader(),100);
 	}
 
-	public void testCreateSessionIntCNPServerString() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#createSession(int, org.ndacm.acmgroup.cnp.CNPServer, java.lang.String)}.
+	 * @throws FailedSessionException 
+	 */
+	@Test
+	public final void testCreateSessionIntCNPServerString() throws FailedSessionException {
+		CNPPrivateSession session = database.createSession(101, server, "monkey");
+		assertEquals(session.getSessionLeader(), 101);
 	}
 
-	public void testRetrieveSessionStringCNPServer() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#retrieveSession(java.lang.String, org.ndacm.acmgroup.cnp.CNPServer)}.
+	 * @throws FailedSessionException 
+	 */
+	@Test
+	public final void testRetrieveSessionStringCNPServer() throws FailedSessionException {
+		CNPSession sess = database.retrieveSession(session1.getSessionName(), server);
+		assertEquals(sess.getClientConnections(),(session1.getClientConnections()));
+		assertEquals(sess.getClientIdToName(), session1.getClientIdToName());
+		assertEquals(sess.getSessionID(), session1.getSessionID());
+		assertEquals(sess.getSessionLeader(), session1.getSessionLeader());
+		assertEquals(sess.getSessionName(), session1.getSessionName());
+		assertEquals(sess.getSourceFiles(), session1.getSourceFiles());
 	}
 
-	public void testRetrieveSessionStringCNPServerString() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#retrieveSession(java.lang.String, org.ndacm.acmgroup.cnp.CNPServer, java.lang.String)}.
+	 * @throws FailedSessionException 
+	 */
+	@Test
+	public final void testRetrieveSessionStringCNPServerString() throws FailedSessionException {
+		CNPPrivateSession sess = database.retrieveSession(session2.getSessionName(), server, "canthackthis");
+		assertEquals(sess.getClientConnections(),(session2.getClientConnections()));
+		assertEquals(sess.getClientIdToName(), session2.getClientIdToName());
+		assertEquals(sess.getSessionID(), session2.getSessionID());
+		assertEquals(sess.getSessionLeader(), session2.getSessionLeader());
+		assertEquals(sess.getSessionName(), session2.getSessionName());
+		assertEquals(sess.getSourceFiles(), session2.getSourceFiles());
 	}
 
-	public void testSessionIsPrivate() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#sessionIsPrivate(java.lang.String)}.
+	 * @throws SQLException 
+	 */
+	@Test
+	public final void testSessionIsPrivate() throws SQLException {
+		assertTrue(database.sessionIsPrivate(session2.getSessionName()));
 	}
 
-	public void testCreateSessionAccountCNPSessionAccountFilePermissionLevelChatPermissionLevel() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#createSessionAccount(org.ndacm.acmgroup.cnp.CNPSession, org.ndacm.acmgroup.cnp.Account, org.ndacm.acmgroup.cnp.Account.FilePermissionLevel, org.ndacm.acmgroup.cnp.Account.ChatPermissionLevel)}.
+	 * @throws SQLException 
+	 */
+	@Test
+	public final void testCreateSessionAccountCNPSessionAccountFilePermissionLevelChatPermissionLevel() throws SQLException {
+		try {
+			database.createSessionAccount(session1, account1, FilePermissionLevel.READ, ChatPermissionLevel.MUTE);
+		} catch (SQLException ex) {
+			fail(ex.getMessage());
+		}
 	}
 
-	public void testDeleteSession() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#deleteSession(org.ndacm.acmgroup.cnp.CNPSession)}.
+	 * @throws FailedSessionException 
+	 * @throws SQLException 
+	 */
+	@Test
+	public final void testDeleteSession() throws FailedSessionException, SQLException {
+		CNPSession sessionToDelete = database.createSession(23, server);
+		database.deleteSession(sessionToDelete);
+		assert(!database.sessionExists(sessionToDelete.getSessionName()));
 	}
 
-	public void testDeleteAccount() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#deleteAccount(org.ndacm.acmgroup.cnp.Account)}.
+	 * @throws SQLException 
+	 */
+	@Test
+	public final void testDeleteAccount() throws SQLException {
+
+		try {
+			Account accountToDelete = database.createAccount("badman", "go@away.com", "icup");
+			database.deleteAccount(accountToDelete);
+		} catch (FailedAccountException ex) {
+			throw new SQLException("Test failed before could actually test account deletion.");
+		}
+
+		try {
+			database.retrieveAccount("badman", "icup");
+			fail("Delete ws not successful.");
+		} catch (FailedAccountException ex) {
+
+		}
+
 	}
 
-	public void testCreateSessionAccountCNPSessionAccountStringFilePermissionLevelChatPermissionLevel() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#createSessionAccount(org.ndacm.acmgroup.cnp.CNPSession, org.ndacm.acmgroup.cnp.Account, java.lang.String, org.ndacm.acmgroup.cnp.Account.FilePermissionLevel, org.ndacm.acmgroup.cnp.Account.ChatPermissionLevel)}.
+	 */
+	@Test
+	public final void testCreateSessionAccountCNPSessionAccountStringFilePermissionLevelChatPermissionLevel() {
+		try {
+			database.createSessionAccount(session2, account2, "canthackthis", FilePermissionLevel.UNRESTRICTED, ChatPermissionLevel.VOICE);
+		} catch (SQLException ex) {
+			fail(ex.getMessage());
+		} catch (FailedSessionException ex) {
+			fail(ex.getMessage());
+		}
 	}
 
-	public void testSessionExists() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#sessionExists(java.lang.String)}.
+	 */
+	@Test
+	public final void testSessionExists() {
+		try {
+			assertTrue(database.sessionExists(session1.getSessionName()));
+			assertFalse(database.sessionExists("23df2$%$#*3"));
+		} catch (Exception ex) {
+			fail();
+		}
 	}
 
-	public void testGetSessionID() {
-		fail("Not yet implemented");
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#getSessionID(java.lang.String)}.
+	 */
+	@Test
+	public final void testGetSessionID() {
+		try {
+			assertEquals(database.getSessionID(session1.getSessionName()), session1.getSessionID());
+		} catch (Exception ex) {
+			fail();
+		}
+	}
+
+	/**
+	 * Test method for {@link org.ndacm.acmgroup.cnp.database.Database#clearTables()}.
+	 */
+	@Test
+	public final void testClearTables() {
+		try {
+			database.clearTables();
+		} catch (SQLException ex) {
+			fail("Fails to work.");
+		}
+		try {
+			database.sessionExists(session1.getSessionName());
+			database.retrieveAccount(account1.getUsername(), "testme");
+			fail("Fails to work correctly.");
+		} catch (Exception ex) {
+
+		}
 	}
 
 }
