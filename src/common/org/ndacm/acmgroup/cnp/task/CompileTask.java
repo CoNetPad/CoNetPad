460c66560fafd59adb7520662710a519c101df07
Justin <swordz@gmail.com>
2012-11-27 20:02:47 -0600

    Added Account jUnit test

--- src/common/org/ndacm/acmgroup/cnp/task/CompileTask.java
+++ src/common/org/ndacm/acmgroup/cnp/task/CompileTask.java
@@ -1,33 +1,58 @@
 package org.ndacm.acmgroup.cnp.task;
 
 import java.util.List;
-
+/**
+ * This is the task for compiling a file
+ * @author Cesar Ramirez
+ *
+ */
 public class CompileTask extends SessionTask {
 
 	protected int userID;
 	protected List<String> sourceFilenames;
 	protected String userAuthToken;
 	
+	/**
+	 * Default Constructor
+	 * @param userID				The unique ID of the user who initialized the compile
+	 * @param sourceFilenames		The list of source files to compile
+	 * @param userAuthToken			The authorized token of the user who initialized 
+	 */
 	public CompileTask(int userID, List<String> sourceFilenames, String userAuthToken) {
 		this.userID = userID;
 		this.sourceFilenames = sourceFilenames;
 		this.userAuthToken = userAuthToken;
 	}
 
+	/**
+	 * This runs the task in a thread
+	 */
 	@Override
 	public void run() {
 		session.executeTask(this);
 		
 	}
 
+	/**
+	 * This gets the Unique ID of the user
+	 * @return	The unique User ID
+	 */
 	public int getUserID() {
 		return userID;
 	}
 
+	/**
+	 * This gets the list of file names that were to be compile
+	 * @return		The list of files to be comipled
+	 */
 	public List<String> getSourceFilenames() {
 		return sourceFilenames;
 	}
-
+	
+	/**
+	 * This gets the user's authorized token
+	 * @return	The user authorized token
+	 */
 	public String getUserAuthToken() {
 		return userAuthToken;
 	}
